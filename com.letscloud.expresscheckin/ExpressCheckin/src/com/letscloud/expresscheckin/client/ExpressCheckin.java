9a519f7f4c9c59addf0e392b27f6b1ec25f30355
CR <csernikr@gmail.com>
2015-03-05 21:06:14 +0100

    Initial

---
+++ com.letscloud.expresscheckin/ExpressCheckin/src/com/letscloud/expresscheckin/client/ExpressCheckin.java
@@ -0,0 +1,14 @@
+package com.letscloud.expresscheckin.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class ExpressCheckin implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
